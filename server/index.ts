bfb61a8e73c6c35c4e366b2da55924aa9babc19c
Hightower111 <116444093+hightower111@users.noreply.github.com>
2025-07-22 11:56:26 +0100

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -32,6 +32,11 @@ app.use((req, res, next) => {
       log(logLine);
     }
   });
+const port = Number(process.env.PORT) || 5000;
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
 
   next();
 });
