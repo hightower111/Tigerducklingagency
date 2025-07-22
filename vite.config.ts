6a7a1a69aec12abe8aad1046534b28a366925023
Hightower111 <116444093+hightower111@users.noreply.github.com>
2025-07-22 19:09:07 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig(async () => {
-  const plugins = [
-    react(),
-    runtimeErrorOverlay(),
-  ];
-
-  if (process.env.NODE_ENV !== "production" && process.env.REPL_ID !== undefined) {
-    const { cartographer } = await import("@replit/vite-plugin-cartographer");
-    plugins.push(cartographer());
-  }
+  const plugins = [react()];
 
   return {
-    root: path.resolve(__dirname, "client"),
     plugins,
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "client", "src"),
-        "@shared": path.resolve(__dirname, "shared"),
-        "@assets": path.resolve(__dirname, "attached_assets"),
-      },
-    },
     build: {
-      outDir: path.resolve(__dirname, "dist/public"),
+      outDir: "dist",
       emptyOutDir: true,
-    },
-    server: {
-      fs: {
-        strict: true,
-        deny: ["**/.*"],
-      },
     },
   };
 });
