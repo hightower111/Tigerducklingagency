9f600294b1421af03ed7245c13340d8205afd758
Hightower111 <116444093+hightower111@users.noreply.github.com>
2025-07-25 17:34:01 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-export default defineConfig(async () => {
-  const plugins = [react()];
-
+export default defineConfig(() => {
   return {
-    plugins,
+    plugins: [react()],
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "src"),
